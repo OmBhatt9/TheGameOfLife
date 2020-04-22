056c8f7e94461695511edc1aff033042d2adfce5
OmBhatt9 <ombb2@illinois.edu>
2020-04-21 22:33:51 -0500

    Added splash screen and botton navigation menu with icons.

---
+++ app/src/main/java/com/example/thegameoflife/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.thegameoflife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent menuIntent = new Intent(SplashScreen.this, MenuActivity.class);
+                    startActivity(menuIntent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
