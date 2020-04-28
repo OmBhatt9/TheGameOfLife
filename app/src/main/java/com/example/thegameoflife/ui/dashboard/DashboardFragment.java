1e1edf4379eb6272d7515f39320bd9a3b233f84e
OmBhatt9 <ombb2@illinois.edu>
2020-04-28 00:54:38 -0500

    Mapped Play Button to new Activity.

--- app/src/main/java/com/example/thegameoflife/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/thegameoflife/ui/dashboard/DashboardFragment.java
@@ -1,9 +1,11 @@
 package com.example.thegameoflife.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,17 +14,21 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.thegameoflife.GamePlayActivity;
 import com.example.thegameoflife.R;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements View.OnClickListener {
 
     private DashboardViewModel dashboardViewModel;
+    Button playButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        playButton = root.findViewById(R.id.playbutton);
+        playButton.setOnClickListener(this);
         final TextView textView = root.findViewById(R.id.text_rulestopic);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -31,5 +37,17 @@ public class DashboardFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.playbutton:
+                Intent playOpen = new Intent(this.getActivity(), GamePlayActivity.class);
+                startActivity(playOpen);
+                break;
+            default:
+                break;
+        }
     }
 }
