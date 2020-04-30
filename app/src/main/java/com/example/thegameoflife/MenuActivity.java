5efa4f690863aeb2719b849fc277c18dd6084828
wyk5436 <wyk5436@outlook.com>
2020-04-30 00:48:50 -0500

    play music switch added. U can switch the music by replacing music file in folder raw in folder res. For now, it's November rain by Guns N Roses, which is one of my favorites :)

--- app/src/main/java/com/example/thegameoflife/MenuActivity.java
+++ app/src/main/java/com/example/thegameoflife/MenuActivity.java
@@ -1,32 +1,40 @@
 package com.example.thegameoflife;
 
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import com.example.thegameoflife.ui.settings.SettingsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MenuActivity extends AppCompatActivity {
+    /**
+     * checking whether the music is playing. It is created as static variable in MenuActivity because
+     * it has the life cycle of the whole MenuActivity.
+     */
+    public static boolean playing = false;
+    /**
+     * player is created here because the BGM has the life cycle of the whole MenuActivity.
+     */
+    public static MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu3);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-        // configuring clicking event on navigation bar
     }
-
 }
--- app/src/main/java/com/example/thegameoflife/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/thegameoflife/ui/settings/SettingsFragment.java
@@ -1,9 +1,13 @@
 package com.example.thegameoflife.ui.settings;
 
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,24 +16,61 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.thegameoflife.MenuActivity;
 import com.example.thegameoflife.R;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class SettingsFragment extends Fragment {
 
     private SettingsViewModel notificationsViewModel;
+    public static Switch musicSwitch;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_settings, container, false);
-//        final TextView textView = root.findViewById(R.id.);
-//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+        final View root = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        // music settings
+        musicSwitch = root.findViewById(R.id.switch2);
+        if (MenuActivity.playing == false) {
+            final SharedPreferences sharedPrefs = getActivity().getSharedPreferences("com.example.xyle", MODE_PRIVATE);
+            musicSwitch.setChecked(sharedPrefs.getBoolean("musicSwitch", false));
+        } else {
+            final SharedPreferences sharedPrefs = getActivity().getSharedPreferences("com.example.xyle", MODE_PRIVATE);
+            musicSwitch.setChecked(sharedPrefs.getBoolean("musicSwitch", true));
+        }
+        // Set a checked change listener for switch button
+        musicSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    playMusic(root);
+                    MenuActivity.playing = true;
+                }
+                else {
+                    pauseMusic(root);
+                    MenuActivity.playing = false;
+                }
+            }
+        });
+
         return root;
+    }
+
+    public void playMusic(View v) {
+        if (MenuActivity.player == null) {
+            MenuActivity.player = MediaPlayer.create(getContext(), R.raw.song);
+            MenuActivity.player.setLooping(true);
+        }
+        MenuActivity.player.start();
+    }
+
+    public void pauseMusic(View v) {
+        if (MenuActivity.player != null) {
+            MenuActivity.player.pause();
+        }
     }
 }
